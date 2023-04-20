ba3047bbbf2a63771300f38fa54ca33174e59c80
Junhyeok Moon <mig070@naver.com>
2023-04-20 16:29:58 +0900

    Add files via upload

---
+++ chatGPT.py
@@ -0,0 +1,22 @@
+import openai
+import discord
+from discord.ext import commands
+
+openai.api_key = "-"
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+messages = []
+
+@bot.command()
+async def 지피티(ctx, *, msg):
+    messages.append({"role":"user", "content":msg})
+
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=messages
+    )
+    chat_response = completion.choices[0].message.content
+    await ctx.send(f'ChatGPT : {chat_response}')
+
+bot.run('-')
